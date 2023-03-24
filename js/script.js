6871819ae453d575b1e4d58ccd9c8e519bd44730
ErrroTwo <80226295+ErrorThree@users.noreply.github.com>
2023-03-24 14:35:25 +0000

    Initial commit

--- js/script.js
+++ js/script.js
@@ -11,10 +11,11 @@ function getRate(from, to) {
 	// This is where you would call an API or get the exchange rate from a database
 	// For the purpose of this example, we'll use some arbitrary rates
 	let rates = {
-		"USD": {"USD": 1.00, "EUR": 0.85, "JPY": 109.94, "GBP": 0.72},
-		"EUR": {"USD": 1.18, "EUR": 1.00, "JPY": 129.70, "GBP": 0.85},
-		"JPY": {"USD": 0.0091, "EUR": 0.0077, "JPY": 1.00, "GBP": 0.0065},
-		"GBP": {"USD": 1.39, "EUR": 1.18, "JPY": 153.22, "GBP": 1.00}
+		"USD": {"USD": 1.00, "EUR": 0.93, "JPY": 130.57, "GBP": 0.82, "CAD": 1.38},
+		"EUR": {"USD": 1.08, "EUR": 1.00, "JPY": 140.36, "GBP": 0.88, "CAD": 1.48},
+		"JPY": {"USD": 0.0077, "EUR": 0.0071, "JPY": 1.00, "GBP": 0.0063, "CAD": 0.011},
+		"GBP": {"USD": 1.22, "EUR": 1.14, "JPY": 159.66, "GBP": 1.00, "CAD": 0.0063},
+    "CAD": {"USD": 0.73, "EUR": 0.68, "JPY": 94.83, "GBP": 0.59, "CAD": 1}
 	};
 	return rates[from][to];
 }
